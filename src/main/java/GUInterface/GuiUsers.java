305ef92a52b10108b7dfe09c6fbd874291bc6123
Alfons Agustí <woodpecker1970@gmail.com>
2022-09-04 15:26:44 +0200

    Starting to develop de GUI users interface

---
+++ src/main/java/GUInterface/GuiUsers.java
@@ -0,0 +1,15 @@
+package GUInterface;
+
+import javax.swing.*;
+
+public class GuiUsers {
+    private JPanel jpMainUsers;
+    private JPanel jpTableListUsers;
+    private JTextField textField2;
+    private JTextField textField1;
+    private JButton btnSave;
+    private JTable table1;
+    private JButton button5;
+    private JButton button3;
+    private JButton button4;
+}
