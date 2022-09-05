b461170814d6410c0956610f015317abc4b311c2
Alfa1970 <woodpecker1970@gmail.com>
2022-09-05 09:05:20 +0200

    GUI Interface table

--- src/main/java/GUInterface/GuiUsers.java
+++ src/main/java/GUInterface/GuiUsers.java
@@ -1,8 +1,9 @@
 package GUInterface;
 
 import javax.swing.*;
+import javax.swing.table.*;
 
-public class GuiUsers {
+public class GuiUsers extends JFrame{
     private JPanel jpMainUsers;
     private JPanel jpTableListUsers;
     private JTextField tfUsername;
@@ -17,4 +18,33 @@ public class GuiUsers {
     private JLabel lbPassword;
     private JPanel jpTable;
     private JPanel jpTableButtons;
+
+    public GuiUsers(){
+        initComponents();
+    }
+
+    private void initComponents() {
+        setContentPane(jpMainUsers);
+        setBounds(0,0,480,720);
+        createTable();
+        setVisible(true);
+    }
+
+    private void createTable(){
+        String[] columns = {"Row","Username", "Password"};
+        String[][] data = {
+                {"1","Woody","9890Ykd"},
+                {"2","Gabri","7852lkj"}
+        };
+        DefaultTableModel model = new DefaultTableModel(data,columns);
+        tbUserList.setModel(model);
+
+        TableColumnModel tc = new DefaultTableColumnModel();
+        tc.setColumnMargin(3);
+
+        JTableHeader th = new JTableHeader(tc);
+        th.setTable(tbUserList);
+
+
+    }
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,12 +1,15 @@
 import DAO.ConnectSQLite;
 import DAO.DAOUser;
 import DAO.DAOUserImp;
+import GUInterface.GuiUsers;
 
 import java.sql.Connection;
 
 public class Test {
 
     public static void main(String[] args) {
+
+        GuiUsers gui = new GuiUsers();
         Connection conn = ConnectSQLite.getConnectSQLite();
         DAOUserImp daoUser = new DAOUserImp();
         daoUser.read();
