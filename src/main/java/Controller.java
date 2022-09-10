27b4faa0550d273d3ac720da86c182f3882cd45e
Alfons Agustí <woodpecker1970@gmail.com>
2022-09-10 20:10:15 +0200

    New comments

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -5,7 +5,7 @@ public class Controller {
 
     public Controller() {
 
-        // TODO -> is needed to implement charge rows from de SQLite user tables
+        // TODO -> is needed to implement charge rows from de SQLite user tabless
 
         GuiUsers guiu = new GuiUsers();
         DAOUserImp dao = new DAOUserImp();
